3e40c3af8948505fdd62056593b6087bd7378131
Osama Mohsen <o.mohsen@shiftebusiness.com>
2017-03-06 13:32:00 +0200

    add new

--- start.php
+++ start.php
@@ -1,19 +1,31 @@
 <?php
 
 
-$con = mysqli_connect("localhost", "root", "root") or die("can't connect");
-$con2 = mysqli_connect("localhost", "root", "root","react") or die("can't connect");
-//$mysqli = new mysqli("localhost", "root", "root", "react");
+//$con = mysqli_connect("localhost", "root", "root") or die("can't connect");
+//$con2 = mysqli_connect("localhost", "root", "root","react") or die("can't connect");
+////$mysqli = new mysqli("localhost", "root", "root", "react");
+//
+//mysqli_select_db($con,'react') or die("can't connectss");
+//
+//$data =[];
+//$results =
+//    mysqli_fetch_array(mysqli_query($con2,"SELECT * from users"));
+//    foreach ($results as $result) {
+//      $data['user'][] = $result;
+//    }
 
-mysqli_select_db($con,'react') or die("can't connectss");
 
-$data =[];
-$results =
-    mysqli_fetch_array(mysqli_query($con2,"SELECT * from users"));
-    foreach ($results as $result) {
-      $data['user'][] = $result;
-    }
 
-var_dump($data);
-// echo  json_encode($result);
-// return $result;
+header('Access-Control-Allow-Origin: *');
+
+header('Access-Control-Allow-Methods: GET, POST');
+
+header("Access-Control-Allow-Headers: X-Requested-With");
+
+
+$users['users'] = [
+    ["id"=>1,"first_name"=>"osana","last_name"=>"mohsen","iamge"=>"my_image"],
+    ["id"=>2,"first_name"=>"amr","last_name"=>"mohamed","iamge"=>"my_image2"]
+];
+$rsut = json_encode($users);
+echo $rsut;
